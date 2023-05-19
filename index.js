bf576625efa28d9c18128ceea44ff65a37aa390c
mehedi hasan <mdchanchal5675@gmail.com>
2023-05-19 22:27:54 +0600

    mongoDB set up done and set env variable with user and pass

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express=require('express')
 const app=express()
 const cors=require('cors')
@@ -13,9 +14,59 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('Car toys server is running')
 })
+//=============
+//MongoDB start
+//=============
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruywwtc.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+//==================================
+//MongoDB CRUD start
+//===================================
 
 
 
+
+
+
+//==================================
+//MongoDB CRUD end
+//===================================
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//==============
+//MongoDB end
 //================
 app.listen(port,()=>{
     console.log(`server is running with port:${port}`);
